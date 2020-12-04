403de720b4e66fa0edeb7b813959fd22ed32dbca
Phanrazak <Phanrazak@yahoo.com>
2020-12-04 23:44:02 +0800

    Updated schedule
    Updated schedule for december

--- commands/schedule.js
+++ commands/schedule.js
@@ -5,11 +5,11 @@ module.exports = {
 	execute(message){
 	  const embed = new Discord.MessageEmbed()
 			.setColor('#FFB9BE')
-			.setURL('https://twitter.com/nishikkuma/status/1322591138809749507')
-			.setTitle('November')
+			.setURL('https://twitter.com/kororon2020/status/1334841986650570753')
+			.setTitle('December')
 			.setTimestamp(Date.now())
 			.setFooter('Suzuna-Chan','https://i.imgur.com/jNqJW4V.png')
-			.setImage('https://pbs.twimg.com/media/ElrJY_jUYAAm9C-.jpg:large');
+			.setImage('https://pbs.twimg.com/media/EoZPfdnVoAAWLMB?format=jpg&name=orig');
 		message.channel.send(embed);
 		
   }
