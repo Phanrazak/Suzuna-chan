9dba6bcda82d8a5e0ce604166f58c91a68464d40
Phanrazak <Phanrazak@yahoo.com>
2021-01-03 14:56:55 +0800

    Update schedule.js

--- commands/schedule.js
+++ commands/schedule.js
@@ -5,11 +5,11 @@ module.exports = {
 	execute(message){
 	  const embed = new Discord.MessageEmbed()
 			.setColor('#FFB9BE')
-			.setURL('https://twitter.com/kororon2020/status/1334841986650570753')
-			.setTitle('December')
+			.setURL('https://pbs.twimg.com/media/EqyTZd7VQAEIX-3?format=png&name=orig')
+			.setTitle('January')
 			.setTimestamp(Date.now())
 			.setFooter('Suzuna-Chan','https://i.imgur.com/jNqJW4V.png')
-			.setImage('https://pbs.twimg.com/media/EoZPfdnVoAAWLMB?format=jpg&name=orig');
+			.setImage('https://pbs.twimg.com/media/EqyTZd7VQAEIX-3?format=png&name=orig');
 		message.channel.send(embed);
 		
   }
