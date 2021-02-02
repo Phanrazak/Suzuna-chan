200cc02886c0db7013fa5ba8181f7b1c8f166e87
Phanrazak <Phanrazak@yahoo.com>
2021-02-02 08:34:26 +0800

    Update schedule.js
    Forgot to add the February lmao

--- commands/schedule.js
+++ commands/schedule.js
@@ -6,7 +6,7 @@ module.exports = {
 	  const embed = new Discord.MessageEmbed()
 			.setColor('#FFB9BE')
 			.setURL('https://twitter.com/fishn_prcn/status/1355874885952630789')
-			.setTitle('January')
+			.setTitle('February')
 			.setTimestamp(Date.now())
 			.setFooter('Suzuna-Chan','https://i.imgur.com/jNqJW4V.png')
 			.setImage('https://i.imgur.com/12cT0jn.png');
