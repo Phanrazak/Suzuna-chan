59201d47efe120d94cb7d6f69451ca12021c23cc
Phanrazak <Phanrazak@yahoo.com>
2021-03-08 16:41:14 +0800

    added Uma Musume support

--- commands/pulltweets.js
+++ commands/pulltweets.js
@@ -1,3 +1,8 @@
+const twit_list = 
+{
+  765105291633405954: "680749508547706887",
+  769070659943235584: "817006794550935572"
+};
 module.exports = {
     name: 'Pulltweet',
 	description: "Grab tweets from priconne redive official twitter, and posts it on discord",
@@ -11,21 +16,19 @@ module.exports = {
                 access_token: Config.accesstoken,
                 access_token_secret: Config.accesssecret,
             })
-            const path = 'statuses/filter'
-            const ids = ['765105291633405954']
-            const params = { follow: ids.join(',') }
-            //const ids = ['id1', 'id2']
-            //const params = { follow: ['765105291633405954'].join(',') }
-            var stream = T.stream(path, params)
+            const path = 'statuses/filter';
+            //const ids = ['765105291633405954']
+            const params = { follow: Object.keys(twit_list).join(',') };
+            var stream = T.stream(path, params);
             stream.on('error', err => {
-                console.log(err)
+                console.log(err);
             })
             stream.on('tweet', async tweet => {
-                if (!ids.includes(tweet.user.id_str)) {
+                if (!Object.keys(twit_list).includes(tweet.user.id_str)) {
                     return
                 }
                 const url = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
-                var channel = await client.channels.fetch('680749508547706887')
+                var channel = await client.channels.fetch(twit_list[tweet.user.id_str])
                 channel.send(url)
             })
         }
