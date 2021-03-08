ab28844bcc33d8ce8678e89a1e4ac4a539a284c6
Phanrazak <Phanrazak@yahoo.com>
2021-03-08 16:58:22 +0800

    Added UEs for IM@S Characters

--- commands/schedule.js
+++ commands/schedule.js
@@ -5,11 +5,11 @@ module.exports = {
 	execute(message){
 	  const embed = new Discord.MessageEmbed()
 			.setColor('#FFB9BE')
-			.setURL('https://github.com/MalitsPlus/ShizuruNotes')
+			.setURL('https://twitter.com/kororon2020/status/1367491286337531906?s=20')
 			.setTitle('March')
 			.setTimestamp(Date.now())
 			.setFooter('Suzuna-Chan','https://i.imgur.com/jNqJW4V.png')
-			.setImage('https://i.imgur.com/UucGzkl.png');
+			.setImage('https://i.imgur.com/D7iw5Lg.png');
 		message.channel.send(embed);
 		
   }
