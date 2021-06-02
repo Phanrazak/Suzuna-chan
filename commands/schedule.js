e7b0253279f86dc224f8292e679428269f59c6d2
Phanrazak <Phanrazak@yahoo.com>
2021-06-02 23:36:55 +0800

    Update schedule.js

--- commands/schedule.js
+++ commands/schedule.js
@@ -3,13 +3,14 @@ module.exports = {
 	name: 'schedule',
 	description: "Schedule for Priconne, will be updated monthly",
 	execute(message){
+		var emote2 = client.emojis.cache.find(emoji => emoji.name === "pepesteer");
 	  const embed = new Discord.MessageEmbed()
 			.setColor('#FFB9BE')
-			.setURL('https://twitter.com/kororon2020/status/1389072005505327106?s=20')
-			.setTitle('May the 4th be with you')
+			.setURL('https://twitter.com/kororon2020/status/1400072263513169926')
+			.setTitle('SSaren this this month'+emote2)
 			.setTimestamp(Date.now())
 			.setFooter('Suzuna-Chan','https://i.imgur.com/jNqJW4V.png')
-			.setImage('https://i.imgur.com/2iUKZCX.png');
+			.setImage('https://i.imgur.com/SJJzc94.png');
 		message.channel.send(embed);
 		
   }
