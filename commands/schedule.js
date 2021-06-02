1ecea3d73999abd18a9626add25a6243d86a26e7
Phanrazak <Phanrazak@yahoo.com>
2021-06-02 23:38:16 +0800

    Update schedule.js

--- commands/schedule.js
+++ commands/schedule.js
@@ -3,11 +3,10 @@ module.exports = {
 	name: 'schedule',
 	description: "Schedule for Priconne, will be updated monthly",
 	execute(message){
-		var emote2 = client.emojis.cache.find(emoji => emoji.name === "pepesteer");
 	  const embed = new Discord.MessageEmbed()
 			.setColor('#FFB9BE')
 			.setURL('https://twitter.com/kororon2020/status/1400072263513169926')
-			.setTitle('SSaren this this month'+emote2)
+			.setTitle('SSaren this this month')
 			.setTimestamp(Date.now())
 			.setFooter('Suzuna-Chan','https://i.imgur.com/jNqJW4V.png')
 			.setImage('https://i.imgur.com/SJJzc94.png');
