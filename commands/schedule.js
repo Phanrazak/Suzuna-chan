efb85078b98bc053390355fc0c0b15d2a4495290
Phanrazak <Phanrazak@yahoo.com>
2021-06-19 10:56:07 +0800

    Updated chara info
    Added Kurumi 6★, Work Mifuyu. and UE for both Wonder Ayumi and Rino

--- commands/schedule.js
+++ commands/schedule.js
@@ -6,7 +6,7 @@ module.exports = {
 	  const embed = new Discord.MessageEmbed()
 			.setColor('#FFB9BE')
 			.setURL('https://twitter.com/kororon2020/status/1400072263513169926')
-			.setTitle('SSaren this this month')
+			.setTitle('SSaren this month')
 			.setTimestamp(Date.now())
 			.setFooter('Suzuna-Chan','https://i.imgur.com/jNqJW4V.png')
 			.setImage('https://i.imgur.com/SJJzc94.png');
