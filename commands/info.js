9a5ab3840651abe7a15adea40235f07a8798c3de
Phanrazak <Phanrazak@yahoo.com>
2021-07-17 20:50:09 +0800

    Updated to Summer Shizuru
    Newest update will be reflected here . Info image will change according to newest character that was added to the game

--- commands/info.js
+++ commands/info.js
@@ -8,7 +8,7 @@ module.exports = {
 			.setAuthor('Phan','https://cdn.discordapp.com/emojis/580237219139289091.png','https://github.com/Phanrazak/Suzuna-chan')
 			.setDescription('I made this :). Click on the link to go to the github to submit any issues or something.')
 			.setFooter('Suzuna-Chan v0.1','https://i.imgur.com/jNqJW4V.png')
-			.setThumbnail('https://i.imgur.com/jNqJW4V.png')
+			.setThumbnail('https://redive.estertion.win/card/full/117131.webp')
 			.setColor('#FFB9BE')
 			.setURL('https://github.com/Phanrazak/Suzuna-chan')
 			.setTitle('ちょす！')
