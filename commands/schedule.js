ca51d07971945c57f7bf321b57e085c957036c08
Phanrazak <Phanrazak@yahoo.com>
2021-08-05 16:47:38 +0800

    Update schedule.js

--- commands/schedule.js
+++ commands/schedule.js
@@ -6,7 +6,7 @@ module.exports = {
 	  const embed = new Discord.MessageEmbed()
 			.setColor('#FFB9BE')
 			.setURL('https://twitter.com/kororon2020/status/1423202098682810370')
-			.setTitle('Half anni soon')
+			.setTitle('I love lolis')
 			.setTimestamp(Date.now())
 			.setFooter('Suzuna-Chan','https://i.imgur.com/jNqJW4V.png')
 			.setImage('https://i.imgur.com/kQY2NWW.png');
