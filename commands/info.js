82213b557ee111729667ea55085ef0a6ee285019
Phanrazak <Phanrazak@yahoo.com>
2021-08-20 21:35:25 +0800

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -8,12 +8,12 @@ module.exports = {
 			.setAuthor('Phan','https://cdn.discordapp.com/emojis/580237219139289091.png','https://github.com/Phanrazak/Suzuna-chan')
 			.setDescription('I made this :). Click on the link to go to the github to submit any issues or something.')
 			.setFooter('Suzuna-Chan v0.1','https://i.imgur.com/jNqJW4V.png')
-			.setThumbnail('https://redive.estertion.win/card/full/104061.webp')
+			.setThumbnail('https://redive.estertion.win/card/full/117331.webp')
 			.setColor('#FFB9BE')
 			.setURL('https://github.com/Phanrazak/Suzuna-chan')
 			.setTitle('ちょす！')
 			.setTimestamp(Date.now())
-			.setImage('https://redive.estertion.win/card/story/106400601.webp');
+			.setImage('https://pbs.twimg.com/media/E9JOB25VIAYsB4W?format=jpg&name=orig');
 		message.channel.send(embed);
   	}
 }
